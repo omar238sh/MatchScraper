51a1c2e2399e94d613673f1a606af1bcbe1db4db
Omar Sherif <127579465+omar238sh@users.noreply.github.com>
2024-06-26 19:54:39 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,28 @@
-use reqwest::{Client, Response};
+#![allow(non_snake_case)]
+
+use reqwest::Client;
 use scraper::{Html, Selector};
-use std::{fmt::format, io};
-use csv::WriterBuilder;
-use std::error::Error;
-use std::fs::OpenOptions;
+#[macro_use] extern crate rocket;
+use rocket::serde::json::Json;
+use serde::Serialize;
 
-#[tokio::main]
-async fn main() {
 
-    let mut date = String::new();
-    println!("please enter date : ");
-    println!("format ==> MM/DD/YYYY ");
-    io::stdin().read_line(&mut date).unwrap();
-    let link = format(format_args!("https://www.yallakora.com/match-center/%D9%85%D8%B1%D9%83%D8%B2-%D8%A7%D9%84%D9%85%D8%A8%D8%A7%D8%B1%D9%8A%D8%A7%D8%AA?date={}#" , date));
-    let client = Client::new();
-    let page = client.get(link).send().await.unwrap();
-    if page.status() == reqwest::StatusCode::OK {
-        println!("We get the site");
-        scrap(page).await;
-    } else {
-        println!("cannot reach the site")
-    }
-    
+#[derive(Serialize)]
+struct MatchResponse {
+    message : String,
+    team1 : String,
+    scores : String,
+    team2 : String
 }
 
-async fn scrap(page : Response) {
+async fn scrap(date : String) -> Vec<[String; 3]> {
+   
+    let mut finalResult: Vec<[String; 3]> = Vec::new();
+
+    let link = format_args!("https://www.yallakora.com/match-center/%D9%85%D8%B1%D9%83%D8%B2-%D8%A7%D9%84%D9%85%D8%A8%D8%A7%D8%B1%D9%8A%D8%A7%D8%AA?date={}#" , date).to_string();
+    let client = Client::new();
+    let page = client.get(link).send().await.unwrap();
+
     let src = page.text().await.unwrap();
     let html = Html::parse_fragment(&src);
     // Selectors
@@ -33,14 +31,6 @@ async fn scrap(page : Response) {
     let teamB = Selector::parse("div.teamB p").unwrap();
     let matchResult = Selector::parse("div.MResult").unwrap();
     let score_span = Selector::parse("span.score").unwrap();
-    let file = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .append(true)
-        .open("data.csv").unwrap();
-
-        
-        let mut csv_writer = WriterBuilder::new().from_writer(file);
 
 
     let matches = html.select(&match_div);
@@ -54,15 +44,36 @@ async fn scrap(page : Response) {
             .flat_map(|result| result.select(&score_span))
             .map(|span| span.inner_html())
             .collect();
-
         
-
-
-        
-
-        csv_writer.write_record(&[teamA_val, scores.join(" - ") , teamB_val]).unwrap();
-        
+        finalResult.push([teamA_val, scores.join("_") ,teamB_val]);
     }
-    csv_writer.flush().unwrap();
+    finalResult
+}
 
+#[get("/<DD>/<MM>/<YYYY>")]
+async fn matches(DD : String , MM : String , YYYY : String) -> Json<Vec<MatchResponse>> {
+
+    let date = format!("{}/{}/{}", MM , DD , YYYY);
+    let a = scrap(date).await;
+
+    let modResponse: Vec<MatchResponse> = a.iter().map(|x| { MatchResponse {
+        message : "connected".to_string(),
+        team1 : x[0].clone(),
+        team2 : x[2].clone(),
+        scores : x[1].clone()
+    } }).collect();
+    Json(modResponse)
+}
+
+#[get("/")]
+fn start() -> String {
+    String::from("Hello! You are using my API to get match data.\n\n\
+                  To fetch match data, use the following format:\n\
+                  /DD/MM/YYYY\n\n\
+                  Example: /16/06/2024")
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![matches , start])
 }
